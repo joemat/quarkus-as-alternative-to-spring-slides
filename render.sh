55ab8951d49d9bd3e856b973bc70dc5f07a27415
Jörg Matysiak <joemat@elinja.de>
2020-06-10 21:32:28 +0200

    * Reviewed and added more slides

--- render.sh
+++ render.sh
@@ -1,6 +1,30 @@
-rm -rf build && mkdir build
+#!/bin/sh
 
-cp -r docs/images build
+clean_build_dir() {
+ rm -rf build && mkdir build
+}
+
+copy_images_to_build_dir()  {
+  cp -r docs/images  build
+}
+
+OFFLINE_MODE=0
+if [ "-for-offline-usage" = "$1" ]
+then
+  OFFLINE_MODE=1
+fi
+
+clean_build_dir
+copy_images_to_build_dir
+
+if [ "$OFFLINE_MODE" -eq 1 ]
+then
+  mkdir build/style
+  curl -L https://github.com/hakimel/reveal.js/archive/3.9.2.tar.gz | tar -zxvf - -C build/style
+  REVEALJSDIR="style/reveal.js-3.9.2"
+else
+  REVEALJSDIR='https://cdn.jsdelivr.net/npm/reveal.js@3.9.2'
+fi
 
 DOCKER_ARGS_ADD=""
 if [ -t 1 ]
@@ -16,7 +40,6 @@ docker run --rm $DOCKER_ARGS_ADD \
        asciidoctor/docker-asciidoctor \
        asciidoctor-revealjs \
        -r asciidoctor-diagram \
-       -a 'revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2' \
+       -a "revealjsdir=$REVEALJSDIR" \
        -D /build \
        index.adoc
-
